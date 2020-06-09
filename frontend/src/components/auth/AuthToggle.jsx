6fcf1f2dc220d72f6c46b72bbaceaa74bfd27e2c
Karan Kumar <nailedITnerd@gmail.com>
2020-06-10 02:37:53 +0530

    Add login/logout toggler with state change

--- frontend/src/components/auth/AuthToggle.jsx
+++ frontend/src/components/auth/AuthToggle.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import { AuthContext } from '../../context/AuthContext';
 
 const AuthToggle = () => {
     const history = useHistory();
+    //will be used to toggle login/logout
+    const { userData, setUserData } = useContext(AuthContext);
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined,
+        });
+        localStorage.setItem("auth-token", "");
+    }
     const redirect = (e) => {
+        //incase of logout
+        if(`${e.target.name}` == '/') logout();
+        //common for all
         history.push(`${e.target.name}`);
-    } 
+    }
     return (
         <div>
-            <button onClick={redirect}  name="/login">login</button>
-            <button onClick={redirect} name="/register">register</button>
-            <button onClick={redirect} name="/">logout</button>
+            {
+                userData.user ? 
+                <button onClick={redirect} name="/">logout</button>
+                :
+                <><button onClick={redirect}  name="/login">login</button>
+                <button onClick={redirect} name="/register">register</button> </> 
+            }
         </div>
     )
 }
